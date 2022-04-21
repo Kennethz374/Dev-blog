5f53f989806032d9fff5cb017f0ba10136204517
kennethzhang <kennethzhang374@gmail.com>
2022-04-21 14:09:50 -0700

    created a banner/header for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import NavBar from "./NavBar/NavBar";
+import NavBar from "./Components/NavBar/NavBar";
+import Home from "./Pages/Home/Home";
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
+			<Home />
 		</div>
 	);
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css"
+import Header from "../../Components/Header/Header"
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            This is the Home page
+        </div>
+    )
+}
+
+export default Home
+
