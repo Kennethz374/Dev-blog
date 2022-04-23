78bd77066048066e0217a2941c942497d413458e
kennethzhang <kennethzhang374@gmail.com>
2022-04-23 00:30:56 -0700

    complete the single post page

--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -6,7 +6,7 @@ const SinglePost = () => {
             <div className="postWrapper">
                 <img src="https://images.unsplash.com/photo-1629904869742-6e8ccbd0f78b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2Zlc3NvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="postImg" className="singlePostImage"/>
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor sit amet consectetur 
+                    Lorem ipsum dolor si
                     <div className="singlePostEdit">
                         <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
                         <i className="singlePostIcon fa-solid fa-trash-can"></i>
@@ -18,7 +18,7 @@ const SinglePost = () => {
                     <span className="singlePostDate">1 hour ago</span>
                 </div>
 
-                <p>
+                <p className="singlePostDes">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit qui, excepturi adipisci deserunt ipsum dolore quos quia magni, ducimus quaerat nobis minus iure sed quisquam? Explicabo veritatis amet magni deserunt!
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit qui, excepturi adipisci deserunt ipsum dolore quos quia magni, ducimus quaerat nobis minus iure sed quisquam? Explicabo veritatis amet magni deserunt!
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit qui, excepturi adipisci deserunt ipsum dolore quos quia magni, ducimus quaerat nobis minus iure sed quisquam? Explicabo veritatis amet magni deserunt!
