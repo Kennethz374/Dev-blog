e92bbb81ca8f5887648fd8e3e1b829b8bd43a3a2
kennethzhang <kennethzhang374@gmail.com>
2022-04-23 22:38:19 -0700

    completed add post WritePage

--- src/Pages/Write/Write.jsx
+++ src/Pages/Write/Write.jsx
@@ -3,10 +3,11 @@ import "./Write.css"
 const Write = () => {
     return (
         <div className="write">
+            <img src="https://images.unsplash.com/photo-1519452575417-564c1401ecc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2Zlc3NvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="header" className="writeImage"/>
             <form  className="writeForm">
                 <div className="writeFormGroup">
                     <label htmlFor="fileInput">
-                        <i class="fa-solid fa-plus"></i>
+                        <i className="writeIcon fa-solid fa-plus"></i>
                     </label>
                     <input type="file" id="fileInput" style={{display:"none"}}/>
                     <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
