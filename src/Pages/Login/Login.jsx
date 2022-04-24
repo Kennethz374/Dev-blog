9105f27615a35a335cbdbea3c37eaa4d8c72e031
kennethzhang <kennethzhang374@gmail.com>
2022-04-23 23:36:59 -0700

    completed login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,8 +2,16 @@ import "./Login.css"
 
 const Login = () => {
     return (
-        <div>
-            This is the login page
+        <div className="login">
+            <span className="loginTitle">Login</span>
+            <form className="loginForm">
+                <label htmlFor="">Email</label>
+                <input type="text" className="loginInput" placeholder="Enter your email"/>
+                <label htmlFor="">Password</label>
+                <input type="password" className="loginInput" placeholder="Enter your password"/>
+                <button className="loginButton">Login</button>
+            </form>
+            <button className="registerButton">Register</button>
         </div>
     )
 }
