1c06731a4b7ca441c16199fe17abb4bf53d8681b
kennethzhang <kennethzhang374@gmail.com>
2022-04-24 00:50:12 -0700

    react router bug fix

--- src/App.js
+++ src/App.js
@@ -6,16 +6,38 @@ import UserProfile from "./Pages/UserProfile/UserProfile";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 function App() {
+	// const user = true;
 	return (
 		<div className="App">
-			<NavBar />
-			{/* <Home /> */}
-			{/* <Post /> */}
-			{/* <Write /> */}
-			{/* <UserProfile /> */}
-			{/* <Login /> */}
-			<Register />
+			<Router>
+				<NavBar />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/posts">
+						<Home />
+					</Route>
+					<Route path="/register">
+						<Register />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/write">
+						<Write />
+					</Route>
+					<Route path="/profile">
+						<UserProfile />
+					</Route>
+					<Route path="/post/:Id">
+						<Post />
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import "./NavBar.css"
+import {
+  Link
+} from "react-router-dom";
+
 
 const NavBar = () => {
+    const user = true
     return (
         <div className="NavBar_Container">
 
@@ -14,17 +19,28 @@ const NavBar = () => {
 
             <div className="center">
                 <ul className="centerList">
-                    <li className="listItem">Home</li>
-                    <li className="listItem">About</li>
-                    <li className="listItem">Contact</li>
-                    <li className="listItem">Write</li>
-                    <li className="listItem">Logout</li>
+                    <li className="listItem"><Link to="/" className="link">Home</Link></li>
+                    <li className="listItem"><Link to="/about" className="link">About</Link></li>
+                    <li className="listItem"><Link to="/contact" className="link">Contact</Link></li>
+                    <li className="listItem"><Link to="/write" className="link">Write</Link></li>
+                    <li className="listItem">{user && "Logout"}</li>
                 </ul>
             </div>
-
+        
             <div className="right">
-                <img src="https://images.unsplash.com/photo-1611459293885-f8e692ab0356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="avatar"
+                {user? (
+                    <img src="https://images.unsplash.com/photo-1611459293885-f8e692ab0356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="avatar"
                 className="right_image"/>
+            ):(
+                <ul className="centerList">
+                    <li className="listItem">
+                        <Link className="link" to="/login">Login</Link>
+                    </li>
+                    <li className="listItem">
+                        <Link className="link" to="/register">Register</Link>
+                    </li>
+                </ul >
+            )}  
                 <i className="searchIcon fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
