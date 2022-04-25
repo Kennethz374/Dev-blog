989d9feb50935f555de9e7dd9ac456198169f75b
kennethzhang <kennethzhang374@gmail.com>
2022-04-25 13:50:42 -0700

    hook up with MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(console.log(`db is connected`))
+	.catch((err) => console.log(err));
 const PORT = 5000;
+
 app.listen(PORT, () => {
 	console.log(`PORT ${PORT} is running `);
 });
