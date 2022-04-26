a04dc3edb0fa8a185c7cf1471bcdadb2727944f9
kennethzhang <kennethzhang374@gmail.com>
2022-04-26 13:47:21 -0700

    add bcrypt to hash user password before storing into DB

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
 const User = require("../models/User");
 
 //Register
 router.post("/register", async (req, res) => {
 	try {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPass = await bcrypt.hash(req.body.password, salt);
+
 		const newUser = new User({
 			username: req.body.username,
 			email: req.body.email,
-			password: req.body.password,
+			password: hashedPass,
 		});
 
 		const user = await newUser.save();
