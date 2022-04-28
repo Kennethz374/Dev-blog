cf05da01a505c2b35dc19037fd7727f127df7ac1
kennethzhang <kennethzhang374@gmail.com>
2022-04-27 23:56:51 -0700

    able to upload images to local files

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +17,23 @@ mongoose
 	.connect(process.env.MONGO_URL)
 	.then(console.log(`db is connected`))
 	.catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+	deestination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	//destination to "image" folder
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
+	//cb, pass filename to front end
+});
+
+//configuration
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	res.status(200).json("File has beeeen uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
