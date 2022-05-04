79d414fa758532799a60966ed381c919dd072fc8
kennethzhang <kennethzhang374@gmail.com>
2022-05-04 00:33:26 -0700

    finish single post update function

--- client/src/Components/SinglePost/SinglePost.jsx
+++ client/src/Components/SinglePost/SinglePost.jsx
@@ -12,6 +12,9 @@ const SinglePost = () => {
 	const [singlePost, setSinglePost] = useState({});
 	const publicFile = "http://localhost:5000/images/";
 	const { user } = useContext(Context);
+	const [title, setTitle] = useState("");
+	const [desc, setDesc] = useState("");
+	const [updateMode, setUpdateMode] = useState(false);
 
 	const handleDelete = async () => {
 		try {
@@ -22,10 +25,23 @@ const SinglePost = () => {
 		} catch (error) {}
 	};
 
+	const handleUpdate = async () => {
+		try {
+			await axios.put(`/posts/${singlePost._id}`, {
+				username: user.username,
+				title,
+				desc,
+			});
+			setUpdateMode(false);
+		} catch (error) {}
+	};
+
 	useEffect(() => {
 		const getPost = async () => {
 			const res = await axios.get(`/posts/${path}`);
 			setSinglePost(res.data);
+			setTitle(res.data.title);
+			setDesc(res.data.desc);
 		};
 		getPost();
 	}, [path]);
@@ -40,18 +56,31 @@ const SinglePost = () => {
 					/>
 				)}
 
-				<h1 className="singlePostTitle">
-					{singlePost.title}
-					{singlePost.username === user?.username && (
-						<div className="singlePostEdit">
-							<i className="singlePostIcon fa-solid fa-pen-to-square"></i>
-							<i
-								className="singlePostIcon fa-solid fa-trash-can"
-								onClick={handleDelete}
-							></i>
-						</div>
-					)}
-				</h1>
+				{updateMode ? (
+					<input
+						type="text"
+						value={title}
+						className="singlePostTitleInput"
+						autoFocus
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				) : (
+					<h1 className="singlePostTitle">
+						{title}
+						{singlePost.username === user?.username && (
+							<div className="singlePostEdit">
+								<i
+									className="singlePostIcon fa-solid fa-pen-to-square"
+									onClick={() => setUpdateMode(true)}
+								></i>
+								<i
+									className="singlePostIcon fa-solid fa-trash-can"
+									onClick={handleDelete}
+								></i>
+							</div>
+						)}
+					</h1>
+				)}
 
 				<div className="singlePostInfo">
 					<span className="singlePostAuthor">
@@ -64,8 +93,20 @@ const SinglePost = () => {
 						{new Date(singlePost.createdAt).toDateString()}
 					</span>
 				</div>
-
-				<p className="singlePostDes">{singlePost.desc}</p>
+				{updateMode ? (
+					<>
+						<textarea
+							value={desc}
+							className="singlePostDesInput"
+							onChange={(e) => setDesc(e.target.value)}
+						/>
+						<button className="singlePostButton" onClick={handleUpdate}>
+							Update
+						</button>
+					</>
+				) : (
+					<p className="singlePostDes">{desc}</p>
+				)}
 			</div>
 		</div>
 	);
